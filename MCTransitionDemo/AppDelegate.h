6ef08aa7f5a50caac8e2b0af2ca68f02346976ce
caohouhong <229336009@qq.com>
2018-04-27 14:56:47 +0800

    Initial Commit

---
+++ MCTransitionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MCTransitionDemo
+//
+//  Created by caohouhong on 2018/4/27.
+//  Copyright © 2018年 caohouhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MCTransitionDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MCTransitionDemo
+//
+//  Created by caohouhong on 2018/4/27.
+//  Copyright © 2018年 caohouhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
